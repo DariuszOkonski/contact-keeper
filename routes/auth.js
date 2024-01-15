2d838d9bac3948c0ca2083d6381a56ece62cf9a4
Dariusz Okoński <darek200180@gmail.com>
2024-01-15 20:36:17 +0100

    add UserScheme

--- routes/auth.js
+++ routes/auth.js
@@ -3,10 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
-const auth = require('../middleware/auth');
 const { check, validationResult } = require('express-validator');
-
-const User = require('../models/User');
 
 // @route   GET  api/auth
 // @desc    Get logged in user
