ef75b0c29f989e4cf89d08e97c1f16271fd6deab
Dariusz Okoński <darek200180@gmail.com>
2024-01-25 18:33:16 +0100

    addadd Navbar, Home and About

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
 
 const App = () => {
   return (
-    <div className='App'>
-      <h1>My App</h1>
-    </div>
+    <Router>
+      <>
+        <Navbar />
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+          </Routes>
+        </div>
+      </>
+    </Router>
   );
 };
 
