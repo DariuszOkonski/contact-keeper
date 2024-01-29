9324bd31288eb6a0e620c6a4fb39a3e27b519a3e
Dariusz Okoński <darek200180@gmail.com>
2024-01-29 22:34:01 +0100

    add context

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import ContactState from './context/contact/ContactState';
 
 const App = () => {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <div className='container'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-          </Routes>
-        </div>
-      </>
-    </Router>
+    <ContactState>
+      <Router>
+        <>
+          <Navbar />
+          <div className='container'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+            </Routes>
+          </div>
+        </>
+      </Router>
+    </ContactState>
   );
 };
 
